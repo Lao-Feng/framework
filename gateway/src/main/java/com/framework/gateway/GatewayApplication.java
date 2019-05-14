543b2c5eec6d694001b606a5950c4932a67d3b8d
fengjie <fengjie@wq99.cn>
2019-05-15 00:32:15 +0800

    网关 优化

--- gateway/src/main/java/com/framework/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/framework/gateway/GatewayApplication.java
@@ -2,18 +2,10 @@ package com.framework.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
-@ComponentScan(basePackages = "com.framework")
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {

f38ad36237fa334f6ebe312c9076d9ceba400a38
fengjie <fengjie@wq99.cn>
2019-05-18 01:13:51 +0800

    替换SpringCloud的版本 Greenwich->Finchley.SR1 因为Greenwich各种坑,比如集成feign的坑/父工程继承的的坑
    做程序狗的快乐就是在解决bug那一瞬间的满足感

--- examples/order/src/main/java/com/framework/order/OrderApplication.java
+++ examples/order/src/main/java/com/framework/order/OrderApplication.java
@@ -5,16 +5,29 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 //@EnableSwagger2
-//@EnableFeignClients
+@EnableFeignClients
 @EnableDiscoveryClient
-//@ComponentScan(basePackages = "com.framework")
+@ComponentScan(basePackages = "com.framework")
 @MapperScan("com.framework.order.dao.mapper")
 public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @RestController
+    static class TestController {
+        @GetMapping("/hello")
+        public String hello(@RequestParam String name) {
+            return "hello " + name;
+        }
     }
 }

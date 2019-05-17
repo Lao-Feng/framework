0ac9d21e074ad53e58f43a4f81f1c015df8e4a52
fengjie <fengjie@wq99.cn>
2019-05-18 00:02:08 +0800

    做程序狗的快乐就是在解决bug那一瞬间的满足感

--- examples/order/src/main/java/com/framework/order/web/controller/Demo.java
+++ examples/order/src/main/java/com/framework/order/web/controller/Demo.java
@@ -1,9 +1,10 @@
 package com.framework.order.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * TODO
+ * demo
  *
  * @author: FengJie
  * @date: 2019/5/10 0:12
@@ -11,7 +12,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class Demo {
 
+    @RequestMapping("test")
     public void test() {
-
+        System.out.println("test");
     }
 }
